dfd58f0b3f4544f04a5f161029a6b400d3aac8d5
ericcnguyen04 <ericc.win@gmail.com>
2023-09-24 17:53:00 -0400

    transferred time details

--- src/App.js
+++ src/App.js
@@ -11,6 +11,28 @@ function App() {
 
   const start = () => console.log("hello")
 
+  let updatedMS = time.ms
+  let updatedS = time.s
+  let updatedM = time.m
+  let updatedH = time.h
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++
+      updatedM = 0
+    }
+    if (updatedS === 60) {
+      updatedM++
+      updatedS = 0
+    }
+    if (updatedMs === 100) {
+      updatedS++
+      updatedMS = 0
+    }
+    updatedMS++;
+    return setTime({ms:0, s:0, m:0, h:0});
+  }
+
   return (
     <div className="main-section">
       <div className='clock-holder'>
