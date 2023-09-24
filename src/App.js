7062aff866eb85507e9533e29f39a501154c0882
ericcnguyen04 <ericc.win@gmail.com>
2023-09-24 17:47:20 -0400

    new start function

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ function App() {
 
   const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
 
+  const start = () => console.log("hello")
+
   return (
     <div className="main-section">
       <div className='clock-holder'>
         <div className='stopwatch'>
           <DisplayComponent time={time}/>
-          <BtnComponent />
+          <BtnComponent start={start}/>
         </div>
       </div>
     </div>
