07d81e43563e2137efca4699cb89bfe517554424
ericcnguyen04 <ericc.win@gmail.com>
2023-09-24 17:43:29 -0400

    display format

--- src/Components/DisplayComponent.js
+++ src/Components/DisplayComponent.js
@@ -1,7 +1,10 @@
 function DisplayComponent(props) {
   return (
     <div>
-        <span>{props.time.h}</span>
+        <span>{(props.time.h >= 10) ? props.time.h : "0" + props.time.h}</span>&nbsp;:&nbsp;
+        <span>{props.time.m}</span>&nbsp;:&nbsp;
+        <span>{props.time.s}</span>&nbsp;:&nbsp;
+        <span>{props.time.ms}</span>&nbsp;:&nbsp;
     </div>
   );
 }
