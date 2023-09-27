cd99b8517916735e8fedea357849016c0d79e9cc
ericcnguyen04 <ericc.win@gmail.com>
2023-09-26 20:44:57 -0400

    import buttons

--- src/Components/TimerDisplay.js
+++ src/Components/TimerDisplay.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 
+import TimerButtons from './TimerButtons'
+
 export default function TimerDisplay (props) {
 
     const [seconds, setSeconds] = useState(0)
