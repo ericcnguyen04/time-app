7ea4cdec3ee36d2643528e029439a38ac493653f
ericcnguyen04 <ericc.win@gmail.com>
2023-09-26 20:19:06 -0400

    router dommed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import DisplayComponent from './Components/DisplayComponent';
 import BtnComponent from './Components/BtnComponent';
 import TimerDisplay from './Components/TimerDisplay';
+import Home from './Components/Home';
 
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -81,32 +82,41 @@ function App() {
   return (
     // change all of app and make home page, each stopwatch and timer should have its own page
 
-    <div className="main-section">
-      <div className='clock-holder'>
-        <div className='stopwatch'>
-          <h1> Stop watch</h1>
-          <DisplayComponent time={time}/>
-          <BtnComponent 
-            start={start}
-            status = {status}
-            stop = {stop}
-            reset = {reset}
-            resume = {resume}
-          />
-        </div>
-      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="stopwatch" element={<DisplayComponent />} />
+          <Route path="timer" element={<TimerDisplay />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
-      <div className='clock-holder'>
-        <div className='stopwatch'> {/* this is timer*/}
-        <h1>Timer</h1>
-          <TimerDisplay 
-            minutes={minutes}
-            seconds={seconds}
-          />
-        </div>
-      </div>
+    // <div className="main-section">
+    //   <div className='clock-holder'>
+    //     <div className='stopwatch'>
+    //       <h1> Stop watch</h1>
+    //       <DisplayComponent time={time}/>
+    //       <BtnComponent 
+    //         start={start}
+    //         status = {status}
+    //         stop = {stop}
+    //         reset = {reset}
+    //         resume = {resume}
+    //       />
+    //     </div>
+    //   </div>
 
-    </div>
+    //   <div className='clock-holder'>
+    //     <div className='stopwatch'> {/* this is timer*/}
+    //     <h1>Timer</h1>
+    //       <TimerDisplay 
+    //         minutes={minutes}
+    //         seconds={seconds}
+    //       />
+    //     </div>
+    //   </div>
+
+    // </div>
   );
 }
 
