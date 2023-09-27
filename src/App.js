3b85f9685e15441e76f6fff25ea5b631fa9d5153
ericcnguyen04 <ericc.win@gmail.com>
2023-09-26 20:37:00 -0400

    timer running

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import './App.css';
 
 import DisplayComponent from './Components/DisplayComponent';
+// import BtnComponent from './Components/BtnComponent';
 import TimerDisplay from './Components/TimerDisplay';
 import Home from './Components/Home';
 
+// import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  // // react variables
+  // const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  // const [interv, setInterv] = useState()
+  // const [status, setStatus] = useState(0)
+
+  // // react functions
+  // const start = () => {
+  //   //console.log("hello")
+  //   run();
+  //   setStatus(1)
+  //   setInterv(setInterval(run, 10));
+  // }
+
+  // const stop = () => {
+  //   clearInterval(interv);
+  //   setStatus(2)
+  // }
+
+  // const reset = () => {
+  //   clearInterval(interv);
+  //   setStatus(0);
+  //   setTime({ms:0, s:0, m:0, h:0});
+  // }
+
+  // const resume = () => start();
+
+  // // react logics
+  // let updatedMS = time.ms
+  // let updatedS = time.s
+  // let updatedM = time.m
+  // let updatedH = time.h
+
+  // const run = () => {
+  //   if (updatedM === 60) {
+  //     updatedH++
+  //     updatedM = 0
+  //   }
+  //   if (updatedS === 60) {
+  //     updatedM++
+  //     updatedS = 0
+  //   }
+  //   if (updatedMS === 100) {
+  //     updatedS++
+  //     updatedMS = 0
+  //   }
+  //   updatedMS++;
+  //   return setTime({ms:updatedMS, s:updatedS, m:updatedM, h:updatedH});
+  // }
+
+  // === !! === !! Stopwatch is up !! === !! ==
+
+  // const [seconds, setSeconds] = useState(0)
+  // const [minutes, setMinutes] = useState(0)
+
+  // let timer;
+  // useEffect(() => {
+
+  //     timer = setInterval(() => {
+  //         setSeconds(seconds + 1);
+
+  //         if (seconds === 59) {
+  //             setMinutes(minutes + 1);
+  //             setSeconds(0)
+  //         }
+  //     }, 1000)
+  // })
+
+  // return () => clearInterval(timer)
+
   return (
     // change all of app and make home page, each stopwatch and timer should have its own page
 
--- src/Components/TimerDisplay.js
+++ src/Components/TimerDisplay.js
@@ -2,14 +2,35 @@ import React, {useState, useEffect} from 'react'
 
 export default function TimerDisplay (props) {
 
+    const [seconds, setSeconds] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+
+    let timer;
+    useEffect(() => {
+        timer = setInterval(() => {
+            setSeconds(seconds + 1);
+
+            if (seconds === 59) {
+                setMinutes(minutes + 1);
+                setSeconds(0)
+            }
+        }, 1000)
+    })
+
     return (
-        <div>
-            <span>{props.minutes}</span> &nbsp;:&nbsp;
-            <span>{props.seconds}</span>
-        {/* <span>{(time.h >= 10) ? time.h : "0" + time.h}</span>&nbsp;:&nbsp; */}
-        {/* <span>{({minutes} >= 10) ? {minutes} : "0" + {minutes}}</span>&nbsp;:&nbsp;
-        <span>{({seconds} >= 10) ? {seconds} : "0" + {seconds}}</span>&nbsp;:&nbsp; */}
-        {/* <span>{(time.ms >= 10) ? time.ms : "0" + time.ms}</span>&nbsp;&nbsp; */}
+        <div className="main-section">
+            <div className='clock-holder'>
+                <div className='stopwatch'>
+                    <h1> Stop watch</h1>
+                        <span>{minutes}</span> &nbsp;:&nbsp;
+                        <span>{seconds}</span>
+                </div>
+            </div>
         </div>
+
+        // {/* <span>{(time.h >= 10) ? time.h : "0" + time.h}</span>&nbsp;:&nbsp; */} */}
+        // {/* <span>{({minutes} >= 10) ? {minutes} : "0" + {minutes}}</span>&nbsp;:&nbsp; */}
+        // {/* <span>{({seconds} >= 10) ? {seconds} : "0" + {seconds}}</span>&nbsp;:&nbsp; */}
+        // {/* <span>{(time.ms >= 10) ? time.ms : "0" + time.ms}</span>&nbsp;&nbsp; */}
     )
 }
